a574846942adddbe3b1deb702ef055a27906754b
VXU1230 <siwei.xu1230@gmail.com>
2019-12-02 20:21:57 -0800

    no message

---
+++ dqn/cart_pole.py
@@ -0,0 +1,168 @@
+import numpy as np
+import tensorflow as tf
+import gym
+import os
+import datetime
+from gym import wrappers
+
+
+class MyModel(tf.keras.Model):
+    def __init__(self, num_states, hidden_units, num_actions):
+        super(MyModel, self).__init__()
+        self.input_layer = tf.keras.layers.InputLayer(input_shape=(num_states,))
+        self.hidden_layers = []
+        for i in hidden_units:
+            self.hidden_layers.append(tf.keras.layers.Dense(
+                i, activation='tanh', kernel_initializer='RandomNormal'))
+        self.output_layer = tf.keras.layers.Dense(
+            num_actions, activation='linear', kernel_initializer='RandomNormal')
+
+    @tf.function
+    def call(self, inputs):
+        z = self.input_layer(inputs)
+        for layer in self.hidden_layers:
+            z = layer(z)
+        output = self.output_layer(z)
+        return output
+
+
+class DQN:
+    def __init__(self, num_states, num_actions, hidden_units, gamma, max_experiences, min_experiences, batch_size, lr):
+        self.num_actions = num_actions
+        self.gamma = gamma
+        self.max_experiences = max_experiences
+        self.min_experiences = min_experiences
+        self.batch_size = batch_size
+        self.model = MyModel(num_states, hidden_units, num_actions)
+        self.optimizer = tf.optimizers.Adam(lr)
+        self.experience = {'s': [], 'a': [], 'r': [], 's2': [], 'done': []}
+
+    def predict(self, inputs):
+        return self.model(np.atleast_2d(inputs.astype('float32')))
+
+    @tf.function
+    def train(self, TargetNet):
+        if len(self.experience['s']) < self.min_experiences:
+            return 0
+        ids = np.random.randint(low=0, high=len(self.experience['s']), size=self.batch_size)
+        states = np.asarray([self.experience['s'][i] for i in ids])
+        actions = np.asarray([self.experience['a'][i] for i in ids])
+        rewards = np.asarray([self.experience['r'][i] for i in ids])
+        states_next = np.asarray([self.experience['s2'][i] for i in ids])
+        dones = np.asarray([self.experience['done'][i] for i in ids])
+        value_next = np.max(TargetNet.predict(states_next), axis=1)
+        targets = np.where(dones, rewards, rewards+self.gamma*value_next)
+
+        with tf.GradientTape() as tape:
+            selected_action_values = tf.math.reduce_sum(
+                self.predict(states) * tf.one_hot(actions, self.num_actions), axis=1)
+            loss = tf.math.reduce_sum(tf.square(targets - selected_action_values))
+        variables = self.model.trainable_variables
+        gradients = tape.gradient(loss, variables)
+        self.optimizer.apply_gradients(zip(gradients, variables))
+
+
+    def get_action(self, states, epsilon):
+        if np.random.random() < epsilon:
+            return np.random.choice(self.num_actions)
+        else:
+            return np.argmax(self.predict(np.atleast_2d(states))[0])
+
+    def add_experience(self, exp):
+        if len(self.experience['s']) >= self.max_experiences:
+            for key in self.experience.keys():
+                self.experience[key].pop(0)
+        for key, value in exp.items():
+            self.experience[key].append(value)
+
+    def copy_weights(self, TrainNet):
+        variables1 = self.model.trainable_variables
+        variables2 = TrainNet.model.trainable_variables
+        for v1, v2 in zip(variables1, variables2):
+            v1.assign(v2.numpy())
+
+
+def play_game(env, TrainNet, TargetNet, epsilon, copy_step):
+    rewards = 0
+    iter = 0
+    copy_step = copy_step
+    done = False
+    observations = env.reset()
+    while not done:
+        action = TrainNet.get_action(observations, epsilon)
+        prev_observations = observations
+        observations, reward, done, _ = env.step(action)
+        rewards += reward
+        if done:
+            reward = -200
+            env.reset()
+
+        exp = {'s': prev_observations, 'a': action, 'r': reward, 's2': observations, 'done': done}
+        TrainNet.add_experience(exp)
+        TrainNet.train(TargetNet)
+        iter += 1
+        if iter % copy_step == 0:
+            TargetNet.copy_weights(TrainNet)
+    return rewards
+
+
+def make_video(env, TrainNet):
+    env = wrappers.Monitor(env, os.path.join(os.getcwd(), "videos"), force=True)
+    rewards = 0
+    steps = 0
+    done = False
+    observation = env.reset()
+    while not done:
+        env.render()
+        action = TrainNet.get_action(observation, 0)
+        observation, reward, done, _ = env.step(action)
+        steps += 1
+        rewards += reward
+    print("Testing steps: {} rewards {}: ".format(steps, rewards))
+
+
+def main():
+    env = gym.make('CartPole-v0')
+    gamma = 0.99
+    copy_step = 25
+    num_states = len(env.observation_space.sample())
+    num_actions = env.action_space.n
+    hidden_units = [200, 200]
+    max_experiences = 10000
+    min_experiences = 100
+    batch_size = 32
+    lr = 1e-2
+    current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    log_dir = 'logs/dqn/' + current_time
+    summary_writer = tf.summary.create_file_writer(log_dir)
+
+    TrainNet = DQN(num_states, num_actions, hidden_units, gamma, max_experiences, min_experiences, batch_size, lr)
+    TargetNet = DQN(num_states, num_actions, hidden_units, gamma, max_experiences, min_experiences, batch_size, lr)
+    N = 10000
+    totalrewards = np.empty(N)
+    for n in range(N):
+        epsilon = 1.0 / np.sqrt(n + 1)
+        total_reward= play_game(env, TrainNet, TargetNet, epsilon, copy_step)
+        totalrewards[n] = total_reward
+        avg_rewards = totalrewards[max(0, n - 100):(n + 1)].mean()
+        with summary_writer.as_default():
+            tf.summary.scalar('episode reward', total_reward, step=n)
+            tf.summary.scalar('running avg reward(100)', avg_rewards, step=n)
+        if n % 100 == 0:
+            print("episode:", n, "episode reward:", total_reward, "eps:", epsilon, "avg reward (last 100):", avg_rewards)
+    print("avg reward for last 100 episodes:", avg_rewards)
+    make_video(env, TrainNet)
+    env.close()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
